e53074d9438c62d4c2d970486ba423cea0793bee
akashdeep007 <beingakscool@gmail.com>
2021-10-16 18:52:44 +0530

    Created First Controller and View

---
+++ WebContent/WEB-INF/view/main-menu.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>My First Page</title>
+</head>
+<body>
+<h2>Spring MVC Demo- Home PA</h2>
+</body>
+</html>
---
+++ src/springdemo/HomeController.java
@@ -0,0 +1,14 @@
+package springdemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+ @RequestMapping("/")
+ public String mainMenuPage()
+ {
+	 return "main-menu";
+ }
+}
